34cb0f8b8c6264c386c127c049e985991f164c1a
guipure <igor778868@gmail.com>
2020-07-03 23:26:47 +0300

    Slider is divided into parts

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,17 +1,17 @@
+import Track from './Track';
+import Thumb from './Thumb';
+import Slider from './Slider';
+
 class View {
-  constructor(public anchor: HTMLElement = document.body) {
+  constructor(public anchor: HTMLElement) {
     this.render();
   }
 
   private render(): void {
-    const template = `
-      <div class="slider">
-        <div class="track"></div>
-        <div class="thumb"></div>
-        <div class="thumb"></div>
-      </div>
-    `;
-    this.anchor.innerHTML = template;
+    new Slider(this.anchor);
+    new Track(this.anchor);
+    new Thumb(this.anchor);
+    new Thumb(this.anchor);
   }
 }
 
