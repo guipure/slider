c971dfcae3627f233cd26c09ccf72ce6d244dff0
guipure <igor778868@gmail.com>
2020-07-04 21:22:12 +0300

    Created restrictions for thumbs

--- src/View/Thumb.ts
+++ src/View/Thumb.ts
@@ -32,16 +32,37 @@ class Thumb {
         throw 'Thumb has no parent element';
       }
       const slider: HTMLElement = thumb.parentElement;
-      const sliderPosition = slider.getBoundingClientRect();
-      return sliderPosition;
+      return slider.getBoundingClientRect();
     }
 
     function moveThumbAt(coordinate: number) {
-      const thumbWidth: number = Number.parseInt(getComputedStyle(thumb).width);
+      const thumbHalfWidth: number = Number.parseInt(getComputedStyle(thumb).width) / 2;
+      let startSliderPosition: number;
+      let endSliderPosition: number;
+      let pageOffset: number;
+      let thumbProp: 'left' | 'top';
+
       if (orientation === 'horizontal') {
-        thumb.style.left = `${coordinate - getSliderPosition().x - pageXOffset - thumbWidth / 2}px`;
+        startSliderPosition = getSliderPosition().left;
+        endSliderPosition = getSliderPosition().right;
+        pageOffset = pageXOffset;
+        thumbProp = 'left';
       } else {
-        thumb.style.top = `${coordinate - getSliderPosition().y - pageYOffset - thumbWidth / 2}px`;
+        startSliderPosition = getSliderPosition().top;
+        endSliderPosition = getSliderPosition().bottom;
+        pageOffset = pageYOffset;
+        thumbProp = 'top';
+      }
+
+      const start: number = startSliderPosition + pageOffset + thumbHalfWidth;
+      const end: number = endSliderPosition + pageOffset - thumbHalfWidth;
+
+      if (coordinate < start) {
+        thumb.style[thumbProp] = '0px';
+      } else if (coordinate > end) {
+        thumb.style[thumbProp] = `${end - start}px`;
+      } else {
+        thumb.style[thumbProp] = `${coordinate - start}px`;
       }
     }
 
