dad33ebb271b0f8cdd21ccd7af6c3d9f38598fe9
guipure <igor778868@gmail.com>
2020-07-05 15:30:33 +0300

    Thumb class refactoring

--- src/View/Thumb.ts
+++ src/View/Thumb.ts
@@ -1,78 +1,71 @@
 import { ViewOptions } from '../Presenter/Options';
+import Slider from './Slider';
 
 class Thumb {
-  constructor(anchor: HTMLElement, private options: ViewOptions) {
-    const slider = anchor.querySelector('.slider');
+  constructor(private slider: Slider, private options: ViewOptions) {
     const thumb = this.createThumb();
-    slider && slider.append(thumb);
+    slider.sliderDiv && slider.sliderDiv.append(thumb);
   }
 
   private createThumb(): HTMLElement {
     const thumb = document.createElement('div');
     thumb.className = `thumb thumb-${this.options.orientation}`;
-    thumb.addEventListener('mousedown', this.thumbHandler);
+    thumb.addEventListener('mousedown', this.onMouseDown.bind(this));
     return thumb;
   }
 
-  private thumbHandler(event: any) {
+  public moveThumbAt(thumb: HTMLElement, coordinate: number, orientation: string): void {
+    const thumbHalfWidth: number = Number.parseInt(getComputedStyle(thumb).width) / 2;
+    let startSliderPosition: number;
+    let endSliderPosition: number;
+    let pageOffset: number;
+    let thumbProp: 'left' | 'top';
+
+    if (orientation === 'horizontal') {
+      startSliderPosition = this.slider.getSliderPosition().left;
+      endSliderPosition = this.slider.getSliderPosition().right;
+      pageOffset = pageXOffset;
+      thumbProp = 'left';
+    } else {
+      startSliderPosition = this.slider.getSliderPosition().top;
+      endSliderPosition = this.slider.getSliderPosition().bottom;
+      pageOffset = pageYOffset;
+      thumbProp = 'top';
+    }
+
+    const start: number = startSliderPosition + pageOffset + thumbHalfWidth;
+    const end: number = endSliderPosition + pageOffset - thumbHalfWidth;
+
+    if (coordinate < start) {
+      thumb.style[thumbProp] = '0px';
+    } else if (coordinate > end) {
+      thumb.style[thumbProp] = `${end - start}px`;
+    } else {
+      thumb.style[thumbProp] = `${coordinate - start}px`;
+    }
+  }
+
+  private onMouseDown(event: any) {
     const thumb: HTMLElement = event.currentTarget;
     const orientation = thumb.classList.contains('thumb-horizontal') ? 'horizontal' : 'vertical';
     const coordinate = getCoordinate(event);
-    moveThumbAt(coordinate);
+    this.moveThumbAt(thumb, coordinate, orientation);
+    const onMouseMove = (event: any) => this.moveThumbAt(thumb, getCoordinate(event), orientation);
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
 
     function getCoordinate(event: any) {
       const coordinate = orientation === 'horizontal' ? event.pageX : event.pageY;
       return coordinate;
-    }
-
-    function getSliderPosition() {
-      if (!thumb.parentElement) {
-        throw 'Thumb has no parent element';
-      }
-      const slider: HTMLElement = thumb.parentElement;
-      return slider.getBoundingClientRect();
-    }
-
-    function moveThumbAt(coordinate: number) {
-      const thumbHalfWidth: number = Number.parseInt(getComputedStyle(thumb).width) / 2;
-      let startSliderPosition: number;
-      let endSliderPosition: number;
-      let pageOffset: number;
-      let thumbProp: 'left' | 'top';
-
-      if (orientation === 'horizontal') {
-        startSliderPosition = getSliderPosition().left;
-        endSliderPosition = getSliderPosition().right;
-        pageOffset = pageXOffset;
-        thumbProp = 'left';
-      } else {
-        startSliderPosition = getSliderPosition().top;
-        endSliderPosition = getSliderPosition().bottom;
-        pageOffset = pageYOffset;
-        thumbProp = 'top';
-      }
-
-      const start: number = startSliderPosition + pageOffset + thumbHalfWidth;
-      const end: number = endSliderPosition + pageOffset - thumbHalfWidth;
-
-      if (coordinate < start) {
-        thumb.style[thumbProp] = '0px';
-      } else if (coordinate > end) {
-        thumb.style[thumbProp] = `${end - start}px`;
-      } else {
-        thumb.style[thumbProp] = `${coordinate - start}px`;
-      }
-    }
-
-    function onMouseMove(event: any) {
-      moveThumbAt(getCoordinate(event));
-    }
+    }        
 
     function onMouseUp() {
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    thumb.ondragstart = function() {
+      return false;
     }
   }
 }
