c97f57f7b03a54fa462c3ebbe2c3151ea3d72e36
guipure <igor778868@gmail.com>
2020-07-06 14:31:18 +0300

    Added checkOptions and calculateValies methods

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -1,8 +1,39 @@
 import { ModelOptions } from '../Presenter/Options';
 
 class Model {
-  constructor(private options: ModelOptions) {
+  private values: number[];
 
+  constructor(private options: ModelOptions) {
+    this.checkOptions()
+    this.values = this.calculateValues();
+    console.log(this.values);
+  }
+
+  private checkOptions(): void {
+    const { min, max, step } = this.options;
+
+    if (min > max) {
+      [this.options.max, this.options.min] = [min, max];
+    }
+
+    if (step <= 0) {
+      throw 'Step must be positive number'
+    }
+  }
+
+  private calculateValues(): number[] {
+    const { min, max, step } = this.options;
+    let values: number[] = [];
+    let currVal = min;
+
+    while (currVal < max) {
+      values.push(currVal);
+      currVal += step;
+    }
+
+    values.push(max);
+
+    return values;
   }
 }
 
