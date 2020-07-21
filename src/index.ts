be9b0dd1b44cc75d8e1c0b81c38b28c87bf29fbd
guipure <igor778868@gmail.com>
2020-07-21 12:39:41 +0300

    Added two sliders on demo page

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import './index.scss';
 import './plugin';
 
-$('.js-slider-wrapper').slider();
+$('.js-first-anchor').slider();
 
-$('.js-another').slider({
+$('.js-second-anchor').slider({
+  orientation: 'horizontal',
+  type: 'single',
+  min: -100000,
+  max: 100000,
+  step: 10,
+  from: -1000,
+  hide_from_to: false,
+  hide_scale: false,
+});
+
+$('.js-third-anchor').slider({
   orientation: 'vertical',
   type: 'double',
-  min: 1,
-  max: 10,
+  min: 0,
+  max: 5000,
   step: 2,
-  from: 1,
-  to: 5,
+  from: 500,
+  to: 1500,
+  hide_from_to: false,
+  hide_scale: false,
+});
+
+$('.js-fourth-anchor').slider({
+  orientation: 'vertical',
+  type: 'single',
+  min: 0,
+  max: 500,
+  step: 25,
+  from: 175,
   hide_from_to: false,
   hide_scale: false,
 });
