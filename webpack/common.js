4d415b352c8a026450f676b21215c24913801036
guipure <igor778868@gmail.com>
2020-08-25 12:45:35 +0300

    Webpack configuration refactoring

---
+++ webpack/common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+function getRules(type) {
+  const scssLoaders = {
+    dev: ['style-loader', 'css-loader', 'sass-loader'],
+    prod: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+  };
+
+  return [
+    {
+      test: /\.(scss)$/,
+      use: scssLoaders[type],
+    },
+    {
+      test: /\.ts$/,
+      use: 'ts-loader',
+    },
+  ];
+}
+
+function getPlugins(type) {
+  return [
+    new HTMLWebpackPlugin({
+      currentEnv: process.env.NODE_ENV,
+      filename: 'index.html',
+      template: './demo/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ].concat(type === 'prod' ? (
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].bundle.css',
+    })
+  ) : []);
+}
+
+const commonConfig = {
+  target: 'web',
+  context: path.resolve(__dirname, '..', 'src'),
+  entry: './demo/index.ts',
+  output: {
+    filename: 'scripts/[name].bundle.js',
+    path: path.resolve(__dirname, '..', 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+};
+
+module.exports = { commonConfig, getRules, getPlugins };
